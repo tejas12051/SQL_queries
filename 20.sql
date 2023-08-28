3456ad75f44fa134431b64187b9f43e2ad0e1ec2
Tejas Patil <92673092+tejas12051@users.noreply.github.com>
2023-08-28 11:10:12 +0530

    Add files via upload

---
+++ 20.sql
@@ -0,0 +1,12 @@
+SELECT
+    B.title AS ProductName,
+    SUM(O.quantity) AS TotalQuantitySold
+FROM
+    books B
+JOIN
+    orders O ON B.book_id = O.book_id
+GROUP BY
+    B.title
+ORDER BY
+    TotalQuantitySold DESC
+LIMIT 3;
